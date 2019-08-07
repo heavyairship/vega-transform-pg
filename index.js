7d3bdf765ab33195079d0aab08ded56f67d5f659
Andrew Fichman <andrew.fichman@gmail.com>
2019-08-07 12:27:49 -0400

    remove query setter since this is defined by vega spec; added options setter

--- index.js
+++ index.js
@@ -12,41 +12,37 @@ export default function VegaTransformPostgres(params) {
   Transform.call(this, [], params);
 }
 
+VegaTransformPostgres.setHttpOptions = function(options) {
+  if(options) {
+    this._options = options;
+    return this;
+  }
+  return this._options;
+}
+
 VegaTransformPostgres.Definition = {
-  type: "VegaTransformPostgres",
+  type: "postgres",
   metadata: { changes: true, source: true },
   params: [{ name: "query", type: "string", required: true }]
 };
 
-VegaTransformPostgres.setQuery = function(query) {
-  this.query = query;
-}
-
 const prototype = inherits(VegaTransformPostgres, Transform);
 
 prototype.transform = async function(_, pulse) {
-  return new Promise((resolve, reject) => {
-    const postData = querystring.stringify({
-      'query': this.query
-    });
-    const options = {
-      hostname: 'localhost',
-      port: 3000,
-      method: 'POST',
-      path: '/query',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
-      }
-    }
-    const req = http.request(options, res => {
+  if(!VegaTransformPostgres._options) {
+    throw Error("Vega Transform Postgres query missing. Assign it with setQuery.");
+  }
+  const result = await new Promise((resolve, reject) => {
+    console.log(_.query);
+    const postData = querystring.stringify({'query': _.query});
+    VegaTransformPostgres._options['Content-Length'] = Buffer.byteLength(postData)
+    const req = http.request(VegaTransformPostgres._options, res => {
       let data = '';
       res.on('data', (chunk) => {
         data += chunk;
       });
       res.on('end', () => {
-        console.log(JSON.parse(data).rows);
-        resolve(data);
+        resolve(JSON.parse(data).rows);
       });
     });
     req.on('error', err => {
@@ -56,4 +52,10 @@ prototype.transform = async function(_, pulse) {
     req.write(postData);
     req.end();
   });
+  console.log(result);
+  result.forEach(ingest);
+  const out = pulse.fork(pulse.NO_FIELDS & pulse.NO_SOURCE);
+  out.rem = this.value;
+  this.value = out.add = out.source = result;
+  return out;
 }
